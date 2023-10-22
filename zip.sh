96a9197701152fdc20f155845aba617f538d3abc
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2023-10-22 17:06:03 -0400

    filter

--- zip.sh
+++ zip.sh
@@ -1,5 +1,6 @@
-rm -r my_deployment_package.zip
 cd package                                      
 zip -r ../my_deployment_package.zip .
 cd ..                                           
 zip my_deployment_package.zip lambda_function.py
+aws -q lambda update-function-code --function-name scriptGen --zip-file fileb://my_deployment_package.zip
+rm -r my_deployment_package.zip
